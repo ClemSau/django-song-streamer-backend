5615348cb37b40558e8dd4179b3c2e4a5c16c1bc
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-10-02 16:47:31 +0200

    Add music views

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\clems\desktop\projets\_sandbox\django song streamer\django-song-streamer-backend\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
