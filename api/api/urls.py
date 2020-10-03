9ab71dcd9f528ed53bc4dcebe012084221fd4af5
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-10-03 16:17:16 +0200

    Add requirements

--- api/api/urls.py
+++ api/api/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('music.urls')),
   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #tgis helps in accessing media with native elements
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
